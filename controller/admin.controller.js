99f51bb25849e2bea9b0c8cd9bd2b812ed6b8050
Diky Efriansyah <dikyefriansyah2@gmail.com>
2023-02-22 10:30:40 +0700

    update admin controller

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -2,14 +2,14 @@ const adminService = require("../service/admin.service.js");
 const jwtUtill = require("../util/jwt.util.js");
 
 exports.createNewAdminApi = async (req, res) => {
-  const admin = await adminService.createUser(req);
+  const admin = await adminService.createAdmin(req);
 
   res.status(201).json({ data: admin });
   console.log(admin);
 };
 
 exports.signAdminApi = async (req, res) => {
-  const admin = await adminService.signInUser(req);
+  const admin = await adminService.signInAdmin(req);
   if (admin) {
     const payloadToken = {
       id: admin.id,
